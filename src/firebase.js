b12a432e6123b85b1923863da831c6e9ee9b67e4
Pedro Barbosa <pedro_dith@hotmail.com>
2022-06-14 16:39:15 -0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJmyTzH-J2KI9mWxDzxw0dQEyE_euqRYU",
+  authDomain: "challenge-5a06f.firebaseapp.com",
+  projectId: "challenge-5a06f",
+  storageBucket: "challenge-5a06f.appspot.com",
+  messagingSenderId: "860036324566",
+  appId: "1:860036324566:web:b713fc89098f4f90ea3d77",
+  measurementId: "G-6HR78FKVV4",
+};
