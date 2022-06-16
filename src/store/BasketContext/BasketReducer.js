c3113a587b6368e6f7622de216d890a3404a50bc
Pedro Barbosa <pedro_dith@hotmail.com>
2022-06-16 19:18:14 -0300

    add function to remove item from the list

--- src/store/BasketContext/BasketReducer.js
+++ src/store/BasketContext/BasketReducer.js
@@ -12,6 +12,22 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+    case "REMOVE_FROM_BASKET":
+      // Findinging the index item in the array
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      };
     default:
       return state;
   }
