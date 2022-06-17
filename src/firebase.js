ab97110611b95491e8000b065c65d8b35419a141
Pedro Barbosa <pedro_dith@hotmail.com>
2022-06-17 16:20:20 -0300

    firebase file configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCJmyTzH-J2KI9mWxDzxw0dQEyE_euqRYU",
   authDomain: "challenge-5a06f.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
   appId: "1:860036324566:web:b713fc89098f4f90ea3d77",
   measurementId: "G-6HR78FKVV4",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
