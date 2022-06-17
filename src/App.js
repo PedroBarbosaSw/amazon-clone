7848614f132ef2c9ec1d6985a208dd6e7fc7b4b4
Pedro Barbosa <pedro_dith@hotmail.com>
2022-06-17 16:19:59 -0300

    apply user authentication when log in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { auth } from "./firebase";
+import { useAuthValue } from "./store/AuthContext/AuthProvider";
 
 function App() {
+  const [{}, dispatch] = useAuthValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("USER IS >>>", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
