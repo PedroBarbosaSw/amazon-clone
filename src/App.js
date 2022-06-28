bacc0c0cfbb4c6408feacba52b3f59a4867cf7d5
Pedro Barbosa <pedro_dith@hotmail.com>
2022-06-28 19:24:42 -0300

    change api address to cloud functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import Payment from "./pages/Payment";
+import Orders from "./pages/Orders";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
@@ -13,10 +14,15 @@ import { useAuthValue } from "./store/AuthContext/AuthProvider";
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import axios from "axios";
 
 const promise = loadStripe(
   "pk_test_51LFOOvEZSxGBm7Ldz8rGcS33oFCGZPnT86ZAfzwYGuMngEemTr7uyAvwiSmU9pvVEt6mQyuvOpfTPzmnnVgZWBqt0053iVwpU6"
 );
+
+axios.defaults.baseURL =
+  "https://us-central1-challenge-5a06f.cloudfunctions.net/api";
+// "http://localhost:5001/challenge-5a06f/us-central1/api";
 
 function App() {
   const [{}, dispatch] = useAuthValue();
@@ -45,6 +51,7 @@ function App() {
         <Routes>
           <Route path="/" element={[<Header />, <Home />]} />
           <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/orders" element={[<Header />, <Orders />]} />
           <Route
             path="/payment"
             element={[
@@ -55,6 +62,7 @@ function App() {
             ]}
           />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={[<Header />, <Home />]} />
         </Routes>
       </div>
     </Router>
