19f279ef96ca46b8fc4e377ced0b6a78602b828f
Pedro Barbosa <pedro_dith@hotmail.com>
2022-06-28 12:16:20 -0300

    add payment route

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
+import Payment from "./pages/Payment";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
+
 import { useAuthValue } from "./store/AuthContext/AuthProvider";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51LFOOvEZSxGBm7Ldz8rGcS33oFCGZPnT86ZAfzwYGuMngEemTr7uyAvwiSmU9pvVEt6mQyuvOpfTPzmnnVgZWBqt0053iVwpU6"
+);
 
 function App() {
   const [{}, dispatch] = useAuthValue();
@@ -35,6 +45,15 @@ function App() {
         <Routes>
           <Route path="/" element={[<Header />, <Home />]} />
           <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route
+            path="/payment"
+            element={[
+              <Header />,
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>,
+            ]}
+          />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
